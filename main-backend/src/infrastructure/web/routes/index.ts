1c69b458f9b202842a5a71fe6d3a29a8bd30ec59
thanaphon0737 <thanapon081366@gmail.com>
2025-06-13 15:03:34 +0700

    feat: api for debug table and metadata

--- main-backend/src/infrastructure/web/routes/index.ts
+++ main-backend/src/infrastructure/web/routes/index.ts
@@ -1,8 +1,8 @@
 import { Router } from "express";
 import healthRoutes from './health.routes'; 
-
+import debugRoutes from './debug.routes';
 const router = Router();
 
 router.use('/api/v1',healthRoutes);
-
+router.use('/api/v1/debug', debugRoutes);
 export  default router;
