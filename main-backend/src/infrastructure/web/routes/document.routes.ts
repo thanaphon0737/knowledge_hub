6265e455f6cb9a5f5074d80d56b48759414be439
thanaphon0737 <thanapon081366@gmail.com>
2025-06-17 14:33:27 +0700

    feat(update doc): implement update doc route

--- main-backend/src/infrastructure/web/routes/document.routes.ts
+++ main-backend/src/infrastructure/web/routes/document.routes.ts
@@ -2,12 +2,13 @@ import { Router } from "express";
 import { authMiddleware } from "../middlewares/auth.middleware";
 import { createDocument } from "../controllers/DocumentController";
 import { getDocumentById, getDocuments } from "../controllers/DocumentController";
+import { updateDocument } from "../controllers/DocumentController";
 const router = Router();
 
 router.post('/documents', authMiddleware,createDocument);
 // Define other document-related routes here, e.g., getDocument, updateDocument, deleteDocument
 router.get('/documents', authMiddleware, getDocuments);
 router.get('/documents/:id', authMiddleware, getDocumentById);
-// Example: router.patch('/documents/:id', authMiddleware, updateDocument);
+router.patch('/documents/:id', authMiddleware, updateDocument);
 // Example: router.delete('/documents/:id', authMiddleware, deleteDocument);
 export default router;
