26590ffd7e733aa107a005a9241cf951234b726b
thanaphon0737 <thanapon081366@gmail.com>
2025-06-25 12:43:09 +0700

    feat: adding router for question chat

--- main-backend/src/infrastructure/web/routes/index.ts
+++ main-backend/src/infrastructure/web/routes/index.ts
@@ -1,20 +1,22 @@
 import { Router } from "express";
-import healthRoutes from './health.routes'; 
-import debugRoutes from './debug.routes';
-import authRoutes from './auth.routes';
-import userRoutes from './user.routes'; 
-import documentRoutes from './document.routes';
-import fileRoutes from './file.routes';
-import internalRoutes from './internal.routes';
-
+import healthRoutes from "./health.routes";
+import debugRoutes from "./debug.routes";
+import authRoutes from "./auth.routes";
+import userRoutes from "./user.routes";
+import documentRoutes from "./document.routes";
+import fileRoutes from "./file.routes";
+import internalRoutes from "./internal.routes";
+import chatRoutes from "./chat.routes";
 const router = Router();
 
-router.use('/api/v1',healthRoutes);
-router.use('/api/v1/debug', debugRoutes);
-router.use('/api/v1/auth', authRoutes);
-router.use('/api/v1/user', userRoutes); 
-router.use('/api/v1',documentRoutes);
-router.use('/api/v1',fileRoutes);
+router.use("/api/v1", healthRoutes);
+router.use("/api/v1/debug", debugRoutes);
+router.use("/api/v1/auth", authRoutes);
+router.use("/api/v1/user", userRoutes);
+router.use("/api/v1", documentRoutes);
+router.use("/api/v1", fileRoutes);
 
-router.use('/api/v1/internal',internalRoutes);
-export  default router;
+router.use("/api/v1", chatRoutes);
+
+router.use("/api/v1/internal", internalRoutes);
+export default router;
