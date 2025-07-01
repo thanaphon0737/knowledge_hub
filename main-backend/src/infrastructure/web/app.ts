040317595691707b946cffaf9e89023a1c8a07d9
thanaphon0737 <thanapon081366@gmail.com>
2025-07-01 17:44:15 +0700

    fix: adding cors for allow cors from nextjs frontend

--- main-backend/src/infrastructure/web/app.ts
+++ main-backend/src/infrastructure/web/app.ts
@@ -1,10 +1,22 @@
 import express, { Application } from 'express';
 import cookieParser from 'cookie-parser';
 import mainRouter from './routes';
-
+import cors from 'cors';
 const app: Application = express();
+
+const corsOption = {
+
+    origin: 'http://localhost:3001',
+    credential: true,
+}
+
+app.use(cors(corsOption));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
 app.use(mainRouter);
+
+
 export default app;
