83fab9eb6d7a81a621903273be152d550ffa1781
thanaphon0737 <thanapon081366@gmail.com>
2025-07-07 15:16:37 +0700

    feat: add chat interactive

--- frontend/front-end-ai-hub/src/app/dashboard/chat/page.tsx
+++ frontend/front-end-ai-hub/src/app/dashboard/chat/page.tsx
@@ -1,9 +1,11 @@
 import ChatBox from "@/app/ui/dashboard/chat/chat-box";
+import ChatInteract from "@/app/ui/dashboard/chat/chat-interactive";
 
 export default function ChatPage() {
   return (
     <>
-      <ChatBox />
+      {/* <ChatBox /> */}
+      <ChatInteract/>
     </>
   );
 }
